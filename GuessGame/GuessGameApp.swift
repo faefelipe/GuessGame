fd964d34ee2363f16cc121343f90b559a11ccff2
Felipe Almeida <devios@gmail.com>
2025-09-02 22:08:43 -0300

    Initial Commit

---
+++ GuessGame/GuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameApp.swift
+//  GuessGame
+//
+//  Created by Felipe Almeida on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
